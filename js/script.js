d3c17cfe592ffab484d406ee98253fb9e70235d1
giovannimariaciclosi <giovannimariaciclosi@gmail.com>
2023-03-06 10:42:49 +0100

    feat: creata funzione di generazione array di num casuali

--- js/script.js
+++ js/script.js
@@ -1,15 +1,18 @@
 /*
 Consegna
-L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
-Ogni cella ha un numero progressivo, da 1 a 100.
-Ci saranno quindi 10 caselle per ognuna delle 10 righe.
-Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
-Bonus
+Copiamo la griglia fatta ieri nella nuova repo e aggiungiamo la logica del gioco (attenzione: non bisogna copiare tutta la cartella dell'esercizio ma solo l'index.html, e le cartelle js/ css/ con i relativi script e fogli di stile, per evitare problemi con l'inizializzazione di git).
+Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe. Attenzione: nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali.
+In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina. Altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
+La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe).
+Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
+BONUS:
 Aggiungere una select accanto al bottone di generazione, che fornisca una scelta tra tre diversi livelli di difficoltà:
-- con difficoltà 1 => 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
-- con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
-- con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
-Consigli del giorno:  :party_wizard:
+difficoltà 1 ⇒ 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
+difficoltà 2 ⇒ 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
+difficoltà 3 ⇒ 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
+SUPERBONUS
+Quando si clicca su una bomba e finisce la partita, il software scopre tutte le bombe nascoste.
+Consigli del giorno: :party_wizard:
 Scriviamo prima cosa vogliamo fare passo passo in italiano, dividiamo il lavoro in micro problemi.
 Ad esempio:
 Di cosa ho bisogno per generare i numeri?
@@ -29,12 +32,6 @@ PSEUDOCODICE
 - Nel ciclo for assegno una variabile nuovo elemento alla funzione createSquare e la appendo al grid container
 - Aggiungo un event listener a questa variabile che al click fa aggiungere una classe ai quadrati
 - Aggiungo il console log che stampa, al click di un quadrato, il suo inner text
-
-
-BONUS
-- prendo l'elemento html difficulty selector
-
-
 */
 
 // prendo l'elemento play button e gli assegno una variabile
@@ -163,7 +160,7 @@ playButtonEl.addEventListener("click", function() {
 // FUNZIONI
 
 /**
- * Fnzione che genera un elemento "div", gli assegna una classe da inserire come parametro "addClass" e inserisce al suo interno una stringa = al parametro "text" assegnato alla funzione
+ * Funzione che genera un elemento "div", gli assegna una classe da inserire come parametro "addClass" e inserisce al suo interno una stringa = al parametro "text" assegnato alla funzione
  * @param {any} text
  * @returns {any}
  */
@@ -178,3 +175,44 @@ function createSquare(text, addClass) {
 
   return newElement;
 }
+
+/**
+ * Funzione che genera un numero random compreso tra i due parametri min e max
+ * @param {any} min
+ * @param {any} max
+ * @returns {any}
+ */
+function randomNumberBetween(min, max) {
+
+  // genero un numero random
+  let random = Math.floor(Math.random() * (max - min + 1) + min)
+
+  // una volta che la nostra funzione viene eseguita, restituisci al suo posto questo valore
+  return random;
+
+};
+
+
+
+
+/**
+ * Funzione che genera un array (di lunghezza "count") di numeri casuali, tutti diversi tra loro e con valori tra 1 e "max"
+ * @param {any} count
+ * @param {any} max
+ * @returns {any}
+ */
+function generateRandomNumbersArray(count, max) {
+  const randomNumberList = [];
+  while (randomNumberList.length < count) {
+    const newRandomNumber = randomNumberBetween(1, max);
+    if (randomNumberList.indexOf(newRandomNumber) == -1) {
+      randomNumberList.push(newRandomNumber);
+    }
+  }
+  return randomNumberList;
+};
+
+console.log(generateRandomNumbersArray(16, 100));
+
+
+
